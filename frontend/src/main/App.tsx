8430105982300491e933913367d08123b39fb196
Lubos Verzich <lve@paymorrow.com>
2022-07-04 15:22:52 +0200

    Reordering

---
+++ frontend/src/main/App.tsx
@@ -0,0 +1,7 @@
+import "styles/App.css"
+
+function App() {
+  return <div>Hello   there<p>asdasd   asd    s</p></div>
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/*.tsx",
+    "./src/component/**/*.tsx",
+    "./src/page/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
